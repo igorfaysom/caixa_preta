c9f7c87f437e69184b5c0c4019a0c91c58bdcdb1
joao neiva neto <joaoneivanetos@gmail.com>
2022-03-21 16:27:56 -0300

    first commit

---
+++ caixa_preta.py
@@ -0,0 +1,79 @@
+from operator import itemgetter
+from pickle import TRUE
+import random
+
+MAX_GENES = 9
+def gerarIndividuo():
+  genes =[]
+  for i in range(MAX_GENES):
+    num = random.randint(0,15)
+    bit = '{0:04b}'.format(num)
+    genes.append(bit)
+  unir_genes = ''
+  individuo = unir_genes.join(genes)
+  return { 'individuo': individuo, 'fitness': 0}
+
+def gerarPopulacao(tam_populacao):
+  pop = []
+  for i in range(tam_populacao):
+    pop.append( gerarIndividuo())
+  return pop  
+
+
+
+def avalicao(pop):
+  
+  for i in range(len(pop)):
+    aci_nuc = list(pop[i]['individuo'])                                                                                 
+    expressao = (9 + int(aci_nuc[1]) * int(aci_nuc[4]) - int(aci_nuc[22]) * int(aci_nuc[13]) 
+    + int(aci_nuc[23]) * int(aci_nuc[3]) - int(aci_nuc[20]) * int(aci_nuc[9]) 
+    + int(aci_nuc[35]) * int(aci_nuc[14]) - int(aci_nuc[10]) *  int(aci_nuc[25]) 
+    + int(aci_nuc[15]) * int(aci_nuc[16]) + int(aci_nuc[2])  * int(aci_nuc[32]) 
+    + int(aci_nuc[27]) * int(aci_nuc[18]) + int(aci_nuc[11]) * int(aci_nuc[33]) 
+    -  int(aci_nuc[30]) *  int(aci_nuc[31]) - int(aci_nuc[21]) * int(aci_nuc[24]) 
+    + int(aci_nuc[34]) * int(aci_nuc[26]) - int(aci_nuc[28]) * int(aci_nuc[6]) 
+    + int(aci_nuc[7]) * int(aci_nuc[12]) - int(aci_nuc[5]) * int(aci_nuc[8]) 
+    + int(aci_nuc[17]) *  int(aci_nuc[19]) - int(aci_nuc[0]) *  int(aci_nuc[29]) 
+    + int(aci_nuc[22])  * int(aci_nuc[3]) + int(aci_nuc[20]) *  int(aci_nuc[14]) 
+    + int(aci_nuc[25]) *  int(aci_nuc[15]) + int(aci_nuc[30]) * int(aci_nuc[11]) 
+    + int(aci_nuc[24]) *  int(aci_nuc[18]) + int(aci_nuc[6]) *  int(aci_nuc[7]) 
+    +  int(aci_nuc[8]) * int(aci_nuc[17]) + int(aci_nuc[0]) *  int(aci_nuc[32]))  
+    pop[i]['fitness'] = expressao
+def selecao(pop:list):
+  
+  pop_ord = sorted(pop,key=itemgetter('fitness'))
+  print(pop_ord)
+  soma_fitness = 0
+  soma_roleta = 0
+  for i in range(len(pop_ord)):
+    soma_fitness  += pop_ord[i]['fitness'] 
+  num_sort=random.randint(1, soma_fitness)
+  print(num_sort)
+  for i in range(len(pop_ord)):
+    soma_roleta  += pop_ord[i]['fitness']
+    if soma_roleta >=  num_sort:
+      return pop_ord[i]
+      break
+      
+  
+def cruzamento(ind1, ind2):
+  genes_filho = []
+
+  n = len( ind1['individuo'] )
+  
+  p = random.randint(0, n-1)
+  print(p)
+  for i in range( n ):
+    if i <= p:
+      genes_filho.append( ind1['individuo'][i])
+    else:
+      genes_filho.append(ind2['individuo'][i])   
+
+  return { 'individuo': genes_filho, 'fitness': 0}       
+  
+
+
+pop = gerarPopulacao(3)
+avalicao(pop)
+print(selecao(pop))
+
