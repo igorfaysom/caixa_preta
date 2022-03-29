4dd46071fa67796e99f40e17813f12bfe290a3db
Igor Gabriel <78803063+igorfaysom@users.noreply.github.com>
2022-03-29 16:43:43 -0300

    mutação

--- caixa_preta.py
+++ caixa_preta.py
@@ -9,8 +9,8 @@ def gerarIndividuo():
     num = random.randint(0,15)
     bit = '{0:04b}'.format(num)
     genes.append(bit)
-  unir_genes = ''
-  individuo = unir_genes.join(genes)
+    unir_genes = ''
+    individuo = unir_genes.join(genes)
   return { 'individuo': individuo, 'fitness': 0}
 
 def gerarPopulacao(tam_populacao):
@@ -41,11 +41,14 @@ def avalicao(pop):
 def roleta(pop_ord):
   soma_fitness = 0
   soma_roleta = 0
-  for i in range(len(pop_ord)):
+
+  for i in range(len(pop_ord)):   
     soma_fitness  += pop_ord[i]['fitness']
-  num_sort=random.randint(1, soma_fitness)
+    num_sort=random.randint(1, soma_fitness)
+  
   for i in range(len(pop_ord)):
     soma_roleta  += pop_ord[i]['fitness']
+      
     if soma_roleta >=  num_sort:
       return i
       break
@@ -58,39 +61,59 @@ def selecao(pop):
     ind1 = roleta(pop_ord)
     ind2 = roleta(pop_ord)
     pares.append({'ind1': pop_ord[ind1], 'ind2':pop_ord[ind2]})
-
+  
   return pares
 
 def cruzamento(ind1, ind2):
   genes_filho = []
 
   n = len( ind1['individuo'] )
-
+  
   p = random.randint(0, n-1)
   print(p)
   for i in range( n ):
-    if i <= p:
-      genes_filho.append( ind1['individuo'][i])
-    else:
-      genes_filho.append(ind2['individuo'][i])
-
+      if i <= p:
+        genes_filho.append( ind1['individuo'][i])
+      else:
+        genes_filho.append(ind2['individuo'][i])
+  
   return { 'individuo': genes_filho, 'fitness': 0}
 
 def cruzamento_unicorte(ind1, ind2):
   genes_filho = []
-
+  
   n = len( ind1['individuo'] )
-
+  
   p = random.randint(0, n-1)
 
   for i in range( n ):
-   if i <= p:
-     genes_filho.append( ind1['individuo'][i])
-   else:
-     genes_filho.append( ind2['individuo'][i])
-
+      
+      if i <= p:
+        genes_filho.append( ind1['individuo'][i])
+      else:
+        genes_filho.append( ind2['individuo'][i])
+  
   return { 'individuo': genes_filho, 'fitness': 0}
 
+""" Mutação """
+
+def mutacao (ind1, ind2):
+  n = len( ind1 ['genes'], ind2['genes'] )
+  
+  p = random.randind(0, n -1)
+
+  mutado = {
+    'genes': ind1['genes'].copy(),
+    'fitnes': 0
+    }
+  
+  if mutado['genes'][p] == 0:
+    mutado['genes'][p] = 1
+  else:
+    mutado['genes'][p] = 0
+      
+  return mutado
+  
 pop = gerarPopulacao(3)
 avalicao(pop)
 print(selecao(pop))
